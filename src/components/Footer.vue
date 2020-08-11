dab0148320639764061cb98d998544023bd79279
Buba-Conteh <contehbuba21@gmail.com>
2020-08-11 17:57:20 +0000

    added the social media links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,31 +2,38 @@
   <div class="footer p-1 px-0 pb-0">
     <div class="text-light d-flex justify-content-center">
       <div class="fa m-2">
-          <a href="">
+          <a href="https://www.facebook.com/buba.conteh.39" target="_blank">
                <i class="fab fa-facebook-square fa-1x text-light"></i>
           </a>
       </div>
      
       <div class="fa m-2">
-           <a href="">
+           <a href="https://www.youtube.com/channel/UCjGd6Y4YSljyrEr-pDyZiMg?view_as=subscriber" target="_blank">
                 <i class="fab fa-youtube-square fa-1x text-light"></i>
            </a>
       </div>
       <div class="fa m-2">
-            <a href="">
+            <a href="https://twitter.com/BubaConteh11" target="_blank">
                 <i class="fab fa-twitter-square fa-1x text-light"></i>
             </a>
       </div>
 
       <div class="fa m-2">
-          <a href="">
-              <a href="">
+          <a href="https://www.linkedin.com/in/buba-conteh-407239190" target="_blank">
+             
                               <i class="fab fa-linkedin fa-1x text-light"></i>
-              </a>
+            
           </a>
       </div>  
       <div class="fa m-2">
-            <p class="text-center ">deverloped by BcTech <i class="fas fa-copyright    "></i> 2002</p>
+        
+              <a href="https://github.com/Buba-Conteh" target="_blank">
+                              <i class="fab fa-github fa-1x text-light"></i>
+              </a>
+        
+      </div>  
+      <div class="fa m-2">
+            <p class="text-center ">deverloped by BcTech <i class="fas fa-copyright    "></i> 2020</p>
       </div>  
       
     </div>
